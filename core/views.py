825975810d4f0d1b9e7e2ec49abd11a86d044aaf
HooLeeUn <juliandote@gmail.com>
2026-03-06 15:20:33 -0500

    feat(auth): return token automatically after user registration

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.db.models import Count, Avg
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework import generics, permissions, status
+from rest_framework.authtoken.models import Token
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -23,6 +24,24 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response(
+            {
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                },
+                "token": token.key,
+            },
+            status=status.HTTP_201_CREATED,
+        )
 
 class UserProfileView(RetrieveAPIView):
     serializer_class = UserProfileSerializer
